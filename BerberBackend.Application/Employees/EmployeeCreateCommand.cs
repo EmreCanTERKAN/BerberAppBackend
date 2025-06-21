74bd100258cd8bdda2771f47015435207d10de8d
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-06-21 19:04:29 +0300

    Application Katmanı Yapım Aşaması

---
+++ BerberBackend.Application/Employees/EmployeeCreateCommand.cs
@@ -0,0 +1,38 @@
+﻿using BerberApp_Backend.Domain.Employees;
+using GenericRepository;
+using Mapster;
+using MediatR;
+using TS.Result;
+
+namespace BerberApp_Backend.Application.Employees;
+public sealed record EmployeeCreateCommand(
+    string FirstName,
+    string LastName,
+    DateOnly BirthOfDate,
+    decimal Salary,
+    PersonelInformation PersonelInformation,
+    Address? Address) : IRequest<Result<string>>;
+
+internal sealed class EmployeeCreateCommandHandler(
+    IEmployeeRepository employeeRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<EmployeeCreateCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(EmployeeCreateCommand request, CancellationToken cancellationToken)
+    {
+        var isEmployeeExists = await employeeRepository
+            .AnyAsync(x => x.PersonelInformation.TCNo == request.PersonelInformation.TCNo, cancellationToken);
+
+        if (isEmployeeExists)
+        {
+            return Result<string>.Failure("Bu Tc daha önce kayıt edilmiş");
+        }
+
+        Employee employee = request.Adapt<Employee>();
+
+        employeeRepository.Add(employee); 
+
+        await unitOfWork.SaveChangesAsync(cancellationToken); 
+
+        return "Personel kaydı başarıyla tamamlandı";
+    }
+}
