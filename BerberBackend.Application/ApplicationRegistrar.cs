752533788e6e49a6559a15756420c94d29fd6aa9
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-06-22 17:18:20 +0300

    Application katmanı hazır

---
+++ BerberBackend.Application/ApplicationRegistrar.cs
@@ -0,0 +1,20 @@
+﻿using BerberApp_Backend.Application.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
+using FluentValidation;
+
+namespace BerberApp_Backend.Application;
+public static class ApplicationRegistrar
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(conf =>
+        {
+            conf.RegisterServicesFromAssembly(typeof(ApplicationRegistrar).Assembly);
+            conf.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        });
+
+        services.AddValidatorsFromAssembly(typeof(ApplicationRegistrar).Assembly);
+
+        return services;
+    }
+}
