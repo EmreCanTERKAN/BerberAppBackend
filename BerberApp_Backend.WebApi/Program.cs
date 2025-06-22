32461e11119080d437614ef1e3b74d79b15660a1
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-06-23 00:58:02 +0300

    Global Rate Limit

--- BerberApp_Backend.WebApi/Program.cs
+++ BerberApp_Backend.WebApi/Program.cs
@@ -1,16 +1,44 @@
-using BerberApp_Backend.Infrastructure;
 using BerberApp_Backend.Application;
+using BerberApp_Backend.Infrastructure;
+using Scalar.AspNetCore;
+using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddCors();
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+builder.Services.AddRateLimiter(options =>
+{
+    options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(context =>
+    RateLimitPartition.GetFixedWindowLimiter("global", _ => new FixedWindowRateLimiterOptions
+    {
+        PermitLimit = 100,
+        QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+        QueueLimit = 100,
+        Window = TimeSpan.FromSeconds(1)
+    }));
+});
+
 
 var app = builder.Build();
 
+app.UseCors(x => x
+.AllowAnyHeader()
+.AllowCredentials()
+.AllowAnyMethod()
+.SetIsOriginAllowed(t => true)
+.SetPreflightMaxAge(TimeSpan.FromMinutes(10)));
+
+app.UseRateLimiter();
+
+app.MapOpenApi();
+app.MapScalarApiReference();
 app.MapDefaultEndpoints();
-
-
+app.MapControllers();
 
 app.Run();
