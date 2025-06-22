8d513f53f6f9c47e2085bd0088f89d2508f2550b
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-06-22 16:19:50 +0300

    GetAllQuery

---
+++ BerberApp_Backend.Domain/Abstractions/EntityDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BerberApp_Backend.Domain.Abstractions;
+public abstract class EntityDto
+{
+    public Guid Id { get; set; }
+    public DateTime CreateAt { get; set; }
+    public DateTime UpdateAt { get; set; }
+    public DateTime DeleteAt { get; set; }
+    public bool IsDeleted { get; set; }
+}
+
